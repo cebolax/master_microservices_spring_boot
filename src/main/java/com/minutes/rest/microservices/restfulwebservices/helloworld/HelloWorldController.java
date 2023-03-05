7b64dea422d5442bbc50c556d27912c4500f4aa0
Guilv <cebolax@pm.com>
2023-03-04 21:32:30 -0300

    Step3 - Returning an Object-JSON

--- src/main/java/com/minutes/rest/microservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/minutes/rest/microservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.minutes.rest.microservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class HelloWorldController {
     // GetMapping(path = "/hello")
     public String helloWorld() {
         return "HelloWorld";
+    }
+
+    @GetMapping(path = "/helloBean")
+    public HelloBean helloBean() {
+        return new HelloBean("my custom message");
     }
 }
