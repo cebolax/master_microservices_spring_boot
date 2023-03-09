94c1db72eb1e99b8bbb4f3a560e890ecfe4d7253
Guilv <cebolax@pm.me>
2023-03-08 23:31:47 -0300

    Step12 - Add CustomResponseEntityExceptionHandler

---
+++ src/main/java/com/minutes/rest/microservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.minutes.rest.microservices.restfulwebservices.exception;
+
+import com.minutes.rest.microservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
+        var details = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(details, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) {
+        var details = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(details, HttpStatus.NOT_FOUND);
+    }
+}
