901f640b3d9bb9d45678eb77b9f1de21ffcde198
Guilv <cebolax@pm.me>
2023-03-11 22:27:07 -0300

    Step19 - Apply i18n

--- src/main/java/com/minutes/rest/microservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/minutes/rest/microservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,17 @@
 package com.minutes.rest.microservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
     // GetMapping(path = "/hello")
@@ -23,5 +31,15 @@ public class HelloWorldController {
     @GetMapping(path = "/helloBean/{name}")
     public HelloBean helloBeanPathVariable(@PathVariable String name) {
         return new HelloBean(String.format("hello, %s", name));
+    }
+
+    @GetMapping(path = "/helloI18n")
+    public String helloI18n() {
+        return messageSource.getMessage(
+                "hello.message",
+                null,
+                "HEllo Hello",
+                LocaleContextHolder.getLocale()
+        );
     }
 }
