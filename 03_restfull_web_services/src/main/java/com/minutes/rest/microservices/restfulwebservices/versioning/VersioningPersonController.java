8e9a0b4a175d7cd6de2bf3ddbc2f4c63cd704068
Guilv <cebolax@pm.me>
2023-03-14 15:47:33 -0300

    Step21 - Add Versioning

--- 03_restfull_web_services/src/main/java/com/minutes/rest/microservices/restfulwebservices/versioning/VersioningPersonController.java
+++ 03_restfull_web_services/src/main/java/com/minutes/rest/microservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+    // URL
 
     @GetMapping(path = "/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
@@ -13,6 +14,43 @@ public class VersioningPersonController {
 
     @GetMapping(path = "/v2/person")
     public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("boa", "bob"));
+    }
+
+    // PARAMS
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonWithParams() {
+        return new PersonV1("Bob bob");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonWithParams() {
+        return new PersonV2(new Name("boa", "bob"));
+    }
+
+    // HEADER
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonWithHeaders() {
+        return new PersonV1("Bob bob");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonWithHeaders() {
+        return new PersonV2(new Name("boa", "bob"));
+    }
+
+
+    // ACCEPT HEADER
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonWithAcceptHeaders() {
+        return new PersonV1("Bob bob");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonWithAcceptHeaders() {
         return new PersonV2(new Name("boa", "bob"));
     }
 }
